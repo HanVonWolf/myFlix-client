cc27f45131c9c05a9a37ebf4bfc6c58a88a4c329
HanVonWolf <hannahwolfenden@outlook.com>
2025-03-06 16:21:34 +0100

    Verification of login details

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,15 +1,33 @@
+import React from "react";
+
 export const LoginView = () => {
-    return (
-      <form>
-        <label>
-          Username:
-          <input type="text" />
-        </label>
-        <label>
-          Password:
-          <input type="password" />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password
+    };
+
+    fetch("https://hannahs-myflix-03787a843e96.herokuapp.com", {
+      method: "POST",
+      body: JSON.stringify(data)
+    });
   };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" />
+      </label>
+      <label>
+        Password:
+        <input type="password" />
+      </label>
+      <button type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
