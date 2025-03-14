cfe0377d27b335c20ebd06da400d841941c59ded
HanVonWolf <hannahwolfenden@outlook.com>
2025-03-14 16:15:03 +0100

    Addition of Card view to movie card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,16 +1,21 @@
+import React from "react";
+import { Button, Card } from "react-bootstrap";
 import PropTypes from "prop-types";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.title}
-      </div>
+      <Card>
+        <Card.Body>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{movie.director}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} variant="link">
+            Open
+            </Button>
+        </Card.Body>
+      </Card>
     );
   };
+
 
   MovieCard.propTypes = {
     movie: PropTypes.shape({
