61604a1daffce9c44442f5f42180fad9b1af1862
HanVonWolf <hannahwolfenden@outlook.com>
2025-03-14 16:57:39 +0100

    Back Button Styling

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,3 +1,8 @@
+import React from "react";
+import Col from 'react-bootstrap/Col';
+
+import './movie-view.scss';
+
 export const MovieView = ({ movie, onBackClick }) => {
     return (
       <div>
@@ -20,7 +25,7 @@ export const MovieView = ({ movie, onBackClick }) => {
           <span>Genre: </span>
           <span>{movie.genre}</span>
         </div>
-        <button onClick={onBackClick}>Back</button>
+        <button onClick={onBackClick} className="back-button" style={{ cursor: "pointer" }}>Back</button>
       </div>
     );
   };
